d7a520068522a7c586c7a62595a7408f97b34c18
sylvia <sylvia@gmail.com>
2023-04-03 05:45:10 -0700

    c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (itr = 0; s[itr] != '\0'; itr++)
 	{
-		for (jtr = 0; accept[jtr] == '\0'; jtr++)
+		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
 		{
 			if (accept[jtr] == '\0')
 				return (itr);
