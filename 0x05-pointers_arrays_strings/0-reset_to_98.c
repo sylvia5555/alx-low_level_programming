24d19763f4a9db985e19813ccd53d019825da1b1
Sylvia <sylviayousif55@gmail.com>
2023-09-19 02:24:58 +0300

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98: return the value to 98
+ *
+ * @n: a pointer to an int
+ * Return: void that means is correct.
+ */
+
+void reset_to_98(int *n)
+{
+
+    *n = 98;
+}
